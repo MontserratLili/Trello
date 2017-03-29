5878b289cb4e8b0d68e1dcccdbde1256797f6fd1
MontserratLili <lauraliligm@gmail.com>
2017-03-29 12:48:00 -0600

    agregando pendientes trello

--- assets/js/trello.js
+++ assets/js/trello.js
@@ -1,12 +1,42 @@
-nuevaLista=[];
-function crearTarjeta(){
+function Tarjeta(titulo, pendiente){
+  this.titulo = titulo;
+  this.pendiente = pendiente;
+}
+var nuevaTarjeta=[];
+var nuevoPendiente=[];
+
+function guardarTitulo(){
   var titulo = document.getElementById("agregarTarjeta").value;
-  var nombreLista = document.createElement("tr") + document.getElementById("agregarTarjeta").value;
+  var nueva = new Tarjeta(titulo);
+  document.getElementById("nuevaTarjeta").innerText= titulo;
+}
 
-  var pendientesLista = document.createElement("td");
+var boton = document.createElement("BUTTON");
+var texto = document.createTextNode("Agregar Pendiente");
+var entrada = document.createElement("input");
+
+function crearPendiente(){
+  boton.appendChild(texto);
+  boton.setAttribute("value", "agregarPendiente");
+  entrada.setAttribute("value", "");
+  boton.setAttribute("onclick", "guardarPendiente()");
+  boton.setAttribute("onclick", "segundoInput()");
+  document.body.appendChild(boton);
+}
+
+function segundoInput(){
+ document.body.appendChild(entrada);
+}
 
 
-  nuevaTarjeta.innerHTML=titulo + "<br>" +nombreLista + "<br>"+ pendientesLista +"<br>";
-
-
+function guardarPendiente(){
+  var pendiente = document.getElementById("entrada");
+  var nueva = new Tarjeta(pendiente);
+  nuevoPendiente.push(pendiente);
+  var lista = document.createElement("ul");
+  var lista2 = document.createElement("li");
+      lista.innerText = pendiente;
+      lista.appendChild(lista2);
+  document.getElementById("nuevaTarjeta").appendChild(lista);
+  lista2.innerText = nuevoPendiente;
 }
