d4fc2635f2202f073a489d5ba42c1c80244ae0e0
MontserratLili <lauraliligm@gmail.com>
2017-03-31 09:43:06 -0600

    llegando al mismo punto pero con objetos

--- assets/js/trello.js
+++ assets/js/trello.js
@@ -1,53 +1,60 @@
 //Se crea la clase tarjeta para con ella crear nuestras tarjetas de Trello
-function Tarjeta(titulo, pendiente){
+function Lista(id, titulo){
   this.titulo = titulo;
-  this.pendiente = pendiente;
+  this.id= id;
+  this.pendiente = [];
 }
 
-var nuevaTarjeta=[];
-var nuevoPendiente=[];
+function Pendiente(id, contenido){
+  this.id=id;
+  this.contenido=contenido;
+}
 
-//Se crea una tarjeta con título
-function guardarTitulo(){
+var listas=[];
+
+function crearLista(){
+  var id = Date.now();
   var titulo = document.getElementById("agregarTarjeta").value;
-  var nueva = new Tarjeta(titulo);
-  document.getElementById("nuevaTarjeta").innerText= titulo;
+  console.log(titulo);
+  var lista = new Lista(id, titulo);
+  listas.push(lista);
+  mostrarLista(lista);
 }
 
-//Al almacenar nuestra nueva tarjeta solo con el título, se crea un nuevo boton para agregar pendientes
-var boton = document.createElement("BUTTON");
-var texto = document.createTextNode("Agregar Pendiente");
-var entrada = document.createElement("input");
+function mostrarLista(lista){
+  console.log(lista);
+  var sectionContainer = document.createElement("section");
+  sectionContainer.id = lista.id;
+  console.log(sectionContainer);
+  var contenedorTarjeta = document.getElementById("contenedorTarjetas");
+  var tituloTarjeta = document.createElement("section");
+  console.log(tituloTarjeta);
+    tituloTarjeta.innerText = lista.titulo;
+    sectionContainer.appendChild(tituloTarjeta);
+    contenedorTarjetas.appendChild(sectionContainer);
+    document.getElementById("contenedorTarjetas").innerHTML=lista.titulo;
 
-//Se le da funcionalidad a dicho boton
-function crearPendiente(){
-  boton.appendChild(texto);
-  boton.setAttribute("value", "agregarPendiente");
-  entrada.setAttribute("value", "");
-  boton.setAttribute("onclick", "guardarPendiente()");
-  boton.setAttribute("onclick", "masPendientes()");
-  document.body.appendChild(boton);
+
+
+  var cajaPendiente = document.createElement("input");
+  var texto = document.createTextNode("Agregar Pendiente");
+  var salto = document.createElement("br");
+  var botonPendiente = document.createElement("button");
+  contenedorTarjeta.appendChild(cajaPendiente);
+  contenedorTarjeta.appendChild(salto);
+  contenedorTarjeta.appendChild(botonPendiente);
+
+  cajaPendiente.type = "text";
+  cajaPendiente.setAttribute = "data-lista-id, lista.id";
+  botonPendiente.type = "submit";
+  botonPendiente.value = "Enviar Pendiente";
+  botonPendiente.onclick = "agregarPendiente()";
+  botonPendiente.setAttribute = "data-lista-id, lista.id";
+  botonPendiente.appendChild(texto);
 }
 
-//Se crea una lista que ordena el título
-var lista;
-function guardarPendiente(){
-  var pendiente = document.getElementById("entrada");
-  lista = document.createElement("ul");
-  document.getElementById("nuevaTarjeta").appendChild(lista);
-}
+function agregarPendiente(){
+  var pendienteNuevo = document.getElementById("Date.now()");
+  console.log(pendienteNuevo);
 
-//Se agregan los pendientes como elementos de la lista
-function masPendientes(){
-  var pendiente = document.getElementById("masTarjetas").value;
-  var lista2 = document.createElement("li");
-  lista2.innerText=pendiente;
-  lista.appendChild(lista2);
-}
-
-//Se muestra la tarjeta en pantalla y se da paso a la creación de nuevas tarjetas
-function imprimirTarjeta(){
-  var comentario = document.getElementById("nuevaTarjeta").value;
-  var clone = comentario.cloneNode(true);
-  document.getElementById("tarjeta").appendChild(clone);
 }
